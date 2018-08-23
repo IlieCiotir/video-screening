71127cac9d3612aba54263d24131fe2db850b985
Ilie Ciotir(Toskv) <dipixelhunter@gmail.com>
2018-08-23 23:56:21 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+function createFromConfig() {
+    var videos = document.getElementById('videos');
+    var overlay = document.getElementById('image');
+    createOverlay(overlay);
+    createVideoElements(videos);
+    document.getElementById('config').remove();
+}
+function createVideoElements(videos) {
+    var holder = document.getElementById('holder');
+    if (holder) {
+        for (var i = 0; i < videos.files.length; i++) {
+            var source = videos.files.item(i);
+            var video = document.createElement('video');
+            video.src = window.URL.createObjectURL(source);
+            video.autoplay = true;
+            video.loop = true;
+            var videoHolder = document.createElement('div');
+            videoHolder.classList.add('video-holder');
+            videoHolder.appendChild(video);
+            holder.appendChild(videoHolder);
+        }
+    }
+}
+function createOverlay(overlay) {
+    var holder = document.getElementById('overlay');
+    var image = document.createElement('img');
+    image.src = window.URL.createObjectURL(overlay.files.item(0));
+    holder.appendChild(image);
+}
+function updateVideos(input) {
+    console.log(input.value);
+}
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+
+function createFromConfig() {
+    const videos = document.getElementById('videos') as HTMLInputElement;
+    const overlay = document.getElementById('image') as HTMLInputElement;
+    createOverlay(overlay);
+    createVideoElements(videos);
+    document.getElementById('config').remove();
+}
+
+function createVideoElements(videos: HTMLInputElement) {
+    const holder = document.getElementById('holder');
+    if (holder) {
+        for (let i = 0; i < videos.files.length; i++) {
+            const source = videos.files.item(i);
+            const video = document.createElement('video');
+            video.src = window.URL.createObjectURL(source);
+            video.autoplay = true;
+            video.loop = true;
+            const videoHolder = document.createElement('div');
+            videoHolder.classList.add('video-holder');
+            videoHolder.appendChild(video);
+            holder.appendChild(videoHolder);
+        }
+    }
+}
+
+function createOverlay(overlay: HTMLInputElement) {
+    const holder = document.getElementById('overlay');
+    const image = document.createElement('img');
+    image.src = window.URL.createObjectURL(overlay.files.item(0));
+    holder.appendChild(image);
+}
+
+function updateVideos(input) {
+    console.log(input.value);
+}
